93e928390ec1e0f37f8f9483f3519ec5bdfdbf00
Stouffi <steff.leb@orange.fr>
2020-02-23 12:18:57 +0100

    Dummy change to test cache of the workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
 import * as React from 'react'
 import { add } from './math'
 
-export const App: React.FC = () => <div>Hello the result is {add(1)(2)}</div>
+export const App: React.FC = () => <div>Hello the result is {add(1)(3)}</div>
