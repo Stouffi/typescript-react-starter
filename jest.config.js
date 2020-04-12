aae5ec79410672bf8f8faa9b8b7029931f30d5a2
Stouffi <steff.leb@orange.fr>
2020-04-12 13:23:30 +0200

    Added ignore patterns for eslint.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,3 @@
-// const { defaults: tsjPreset } = require('ts-jest/presets');
-// const { jsWithTs: tsjPreset } = require('ts-jest/presets');
-// const { jsWithBabel: tsjPreset } = require('ts-jest/presets');
-
 module.exports = {
   preset: 'ts-jest/presets/js-with-babel',
   transformIgnorePatterns: ['node_modules/(?!fp-ts)'],
